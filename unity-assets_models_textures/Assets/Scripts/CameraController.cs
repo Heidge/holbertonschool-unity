f0438e8d70f1ce24f07b9ee8d68d429bc4cca5f8
Heidge <axel.gore35@gmail.com>
2023-09-20 16:57:45 +0200

    modifications in camera and plyaer script

--- unity-assets_models_textures/Assets/Scripts/CameraController.cs
+++ unity-assets_models_textures/Assets/Scripts/CameraController.cs
@@ -3,11 +3,36 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform player;
+    private Transform cam;
     public Vector3 offset;
-    // Update is called once per frame
 
-    void Update()
+    public float smooth = 0.5f;
+    public bool lookAtPlayer = false;
+    public bool rotateAroundPlayer = true;
+    public float rotationSpeed = 2.0f;
+
+
+    void Start()
     {
         transform.position = player.position + offset;
+        
+
+    }
+    void Update()
+    {
+        if (rotateAroundPlayer)
+        {
+            Quaternion camTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationSpeed, Vector3.up);
+            offset = camTurnAngle * offset;
+        }
+        
+        Vector3 newPos = player.position + offset;
+        transform.position = Vector3.Slerp(transform.position, newPos, smooth);
+        //player.transform.Rotate(0.0f, transform.rotation.y, 0.0f, Space.Self);
+
+        if (lookAtPlayer || rotateAroundPlayer)
+        {
+            transform.LookAt(player);
+        }
     }
 }
--- unity-assets_models_textures/Assets/Scripts/PlayerController.cs
+++ unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
@@ -11,14 +12,13 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 5.0f;
     public float gravity = -25.0f;
     Vector3 direction;
-
-    CharacterController controller;
+    public CharacterController controller;
+    public Transform player;
 
     // Use this for initialization
     void Start()
     {
-        controller = GetComponent<CharacterController>();
-
+        
     }
 
     void Update()
@@ -32,8 +32,13 @@ public class PlayerController : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 direction.y = jumpForce;
-                Debug.Log("jump ok");
             }
+        }
+
+        if (player.position.y <= -10.0f)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            player.position = new Vector3(player.position.x, player.position.y * 20.0f, player.position.z);
         }
         direction.y += gravity * Time.deltaTime;
         controller.Move(direction * speed * Time.deltaTime);
