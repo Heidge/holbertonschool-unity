cef6ddeec7e7c28b9003e8a019b10cbd2db6d599
Heidge <axel.gore@outlook.fr>
2023-10-12 15:04:07 +0200

    Change skyboxes for scene2 and scene3

---
+++ unity-assets_ui/Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text Timer_Text;
+    private float tempsActuel = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempsActuel += Time.deltaTime;
+        Timer_Text.text = FormatTemps(tempsActuel);
+    }
+
+    string FormatTemps(float temps)
+    {
+        int minutes = Mathf.FloorToInt(temps / 60);
+        int secondes = Mathf.FloorToInt(temps % 60);
+        float centisecondes = (temps * 100) % 100;
+
+        return string.Format("{0:00}:{1:00}:{2:00}", minutes, secondes, centisecondes);
+    }
+}
